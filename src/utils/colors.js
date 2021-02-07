c7b2ec163ea9b2ae0ff0625358fc6fd9a4cbb09e
mrLuisFer <lolesuncrak@gmail.com>
2021-02-07 13:42:35 -0600

    feat: more utils colors added

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -5,12 +5,17 @@ const colors = {
   lemon: "#c7ffd8",
   pink: "#fbaccc",
   white: "#f4f9f9",
-  yellow: "#ffee93",
-  purple: "#845ec2",
+  yellow: "#ffd369",
+  purple: "#a685e2",
+  darkPurple: "#6155a6",
   green: "#a6f0c6",
   cream: "#ffcda3",
   wine: "#cd5d7d",
   orange: "#ff8e71",
+  black: "#171515",
+  cyan: "#cae4db",
+  grey: "#8d93ab",
+  salmon: "#ff7171",
 }
 
 module.exports = colors
